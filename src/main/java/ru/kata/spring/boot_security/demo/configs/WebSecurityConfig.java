46cf4983b7df7e7dd246ac869e03e3e6637d0d85
RecccPS <Vanya3a2a1a@gamil.com>
2024-01-15 20:15:30 +0300

    V1

---
+++ src/main/java/ru/kata/spring/boot_security/demo/configs/WebSecurityConfig.java
@@ -0,0 +1,55 @@
+package ru.kata.spring.boot_security.demo.configs;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import ru.kata.spring.boot_security.demo.service.UserServiceImp;
+
+@Configuration
+@EnableWebSecurity
+@ComponentScan("ru.kata.spring.boot_security.demo")
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    UserServiceImp userService;
+
+
+
+    SuccessUserHandler successUserHandler;
+
+    public WebSecurityConfig(SuccessUserHandler successUserHandler, UserServiceImp userService) {
+        this.successUserHandler = successUserHandler;
+        this.userService = userService;
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/addUser", "/editUser", "/saveUser", "/admin").hasRole("ADMIN")
+                .antMatchers("/login").permitAll()
+                .anyRequest().hasAnyRole("USER", "ADMIN")
+                .and()
+                .formLogin().successHandler(successUserHandler)
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
+    }
+
+    @Autowired
+    protected void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userService).passwordEncoder(bCryptPasswordEncoder());
+    }
+}
---
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
@@ -0,0 +1,85 @@
+package ru.kata.spring.boot_security.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import ru.kata.spring.boot_security.demo.model.Role;
+import ru.kata.spring.boot_security.demo.model.User;
+import ru.kata.spring.boot_security.demo.service.RoleService;
+import ru.kata.spring.boot_security.demo.service.UserServiceImp;
+
+import javax.validation.Valid;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Controller
+public class AdminController {
+    @Autowired
+    private UserServiceImp userService;
+
+    @Autowired
+    private RoleService roleService;
+    @GetMapping("/admin")
+    @Secured("ROLE_ADMIN")
+    public String displayAllUsers(Model model) {
+        model.addAttribute("userList", userService.getAllUsers());
+        return "admin";
+    }
+    @GetMapping("/admin/addUser")
+    @Secured("ROLE_ADMIN")
+    public String displayNewUserForm(Model model) {
+        model.addAttribute("roles", roleService.getAllRoles());
+        model.addAttribute("headerMessage", "Добавить пользователя");
+        model.addAttribute("user", new User());
+        return "addUser";
+    }
+
+    @PostMapping("/admin/editUser")
+    @Secured("ROLE_ADMIN")
+    public String updateUsers(@ModelAttribute("user") User user, @RequestParam(value = "nameRoles", required = false) String[] roles) {
+        userService.getUserAndRoles(user, roles);
+        userService.saveUser(user);
+        return "redirect:/admin";
+    }
+
+    @GetMapping("/admin/editUser")
+    @Secured("ROLE_ADMIN")
+    public String displayEditUserForm(@RequestParam("id") Long id, Model model) {
+        User user = userService.getUserById(id);
+        model.addAttribute("roles", roleService.getAllRoles());
+        model.addAttribute("headerMessage", "Изменить пользователя");
+        model.addAttribute("user", user);
+        return "editUser";
+    }
+
+    @PostMapping("/admin/addUser")
+    @Secured("ROLE_ADMIN")
+    String create(@ModelAttribute("user") User user, @RequestParam(name = "roles", required = false) List<Long> roleId) {
+        userService.getNotNullRole(user);
+        userService.saveUser(user);
+        return "redirect:/admin";
+    }
+
+    @GetMapping("/admin/deleteUser")
+    @Secured("ROLE_ADMIN")
+    public String deleteUserById(@RequestParam("id") Long id) {
+        userService.deleteUser(id);
+        return "redirect:/admin";
+    }
+
+
+    @PostMapping("/admin")
+    public String  deleteUser(@RequestParam(required = true, defaultValue = "" ) Long userId,
+                              @RequestParam(required = true, defaultValue = "" ) String action,
+                              Model model) {
+        if (action.equals("delete")){
+            userService.deleteUser(userId);
+        }
+        return "redirect:/admin";
+    }
+
+}
